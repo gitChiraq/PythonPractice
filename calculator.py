f14678a22f53d8a9c217296a760ad98592f46b81
gitChiraq <mbrinkmann04@gmail.com>
2023-11-11 21:39:08 +0100

    error fixing

--- calculator.py
+++ calculator.py
@@ -1,26 +1,52 @@
 print("First Number")
-num1 = float(input())
+num1 = input()
+
+def is_float(string):
+    try: 
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+while is_float(num1) is False:
+    print("Invalid input")
+    print("First Number")
+    num1 = input()
+
+print(float(num1))
 
 print("Second Number")
 num2 = float(input())
 
+while is_float(num2) is False:
+    print("Invalid input")
+    print("First Number")
+    num1 = input()
+
 print("Pick Operator: + , - , * , / , %")
 operator = input()
+while True:
+    if operator == "+":
+        print(float(num1) + float(num2))
+        break
 
-if operator == "+":
-    print(num1 + num2)
+    elif operator == "-":
+        print(float(num1) - float(num2))
+        break
 
-elif operator == "-":
-    print(num1 - num2)
+    elif operator == "*":
+        print(float(num1) * float(num2))
+        break
 
-elif operator == "*":
-    print(num1 * num2)
+    elif operator == "/":
+        print(float(num1) / float(num2))
+        break
 
-elif operator == "/":
-    print(num1 / num2)
+    elif operator == "%":
+        print(float(num1) % float(num2))
+        break
 
-elif operator == "%":
-    print(num1 % num2)
-
-else:
-    print("Invalid Input")
+    else:
+        print("Invalid Input")
+        print("Pick Operator: + , - , * , / , %")
+        operator = input()
